30f8a893d80afca863aa42b9969e91add3659247
vins-stha <shrs_samar@yahoo.com>
2022-06-04 18:52:16 +0300

    solutin 1-12,13,14

--- TMCProjects/mooc-java-programming-i/part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
+++ TMCProjects/mooc-java-programming-i/part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
@@ -7,6 +7,11 @@ public class DoubleInput {
         Scanner scanner = new Scanner(System.in);
 
         // write your program here
+        System.out.println("Give a number:");
+
+        double value = Double.valueOf(scanner.nextLine());
+
+        System.out.println("You gave the number " + value);
 
     }
 }
--- TMCProjects/mooc-java-programming-i/part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
+++ TMCProjects/mooc-java-programming-i/part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
@@ -7,6 +7,9 @@ public class BooleanInput {
         Scanner scanner = new Scanner(System.in);
 
         // write your program here
+        System.out.println("Write something:");
+        boolean value = Boolean.valueOf(scanner.nextLine());
+        System.out.println("True or false? " + value);
 
     }
 }

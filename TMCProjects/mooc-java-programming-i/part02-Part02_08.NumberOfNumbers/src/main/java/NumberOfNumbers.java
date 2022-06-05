1050876aa621845da7ee718badf01d93027d8363
vins-stha <shrs_samar@yahoo.com>
2022-06-05 12:24:25 +0300

    part02 repeating functionality competed

--- TMCProjects/mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ TMCProjects/mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -6,5 +6,20 @@ public class NumberOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int inputCount = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+            inputCount = inputCount + 1;
+        }
+
+        System.out.println("Number of numbers: " + inputCount);
+
     }
 }
--- TMCProjects/mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ TMCProjects/mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,22 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+        double avg = 0;
+        while (true) {
+            System.out.println("Give a number:");
+
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == 0) {
+                break;
+            }
+
+            sum += number;
+            count = count +1;
+        }
+        System.out.println("Average of the numbers: " + ((double)sum / count));
 
     }
 }

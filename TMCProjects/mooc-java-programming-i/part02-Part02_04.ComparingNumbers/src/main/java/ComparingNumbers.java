4dddac58bbf5acc21113a195e3d0bcf430a00a9c
vins-stha <shrs_samar@yahoo.com>
2022-06-05 11:05:32 +0300

    Recurring problems and patterns to solve them-completed

--- TMCProjects/mooc-java-programming-i/part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
+++ TMCProjects/mooc-java-programming-i/part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
@@ -6,5 +6,17 @@ public class ComparingNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Declaring the variables and assigning user input to them
+        int first = Integer.valueOf(scanner.nextLine());
+        int second = Integer.valueOf(scanner.nextLine());
+
+        if (first > second) { // if the sum is over 100
+            System.out.println(first + " is greater than " + second + ".");
+        } else if (first < second) { // if the sum is less than 0
+            System.out.println(first + " is smaller than " + second + ".");
+        } else { // otherwise
+            System.out.println(first + " is equal to " + second + ".");
+        }
+
     }
 }

d4a6696bcd3b4c7e1fb69e5efc3954477977c60d
vins-stha <shrs_samar@yahoo.com>
2022-06-05 13:13:39 +0300

    part 1-02 - more loops

--- TMCProjects/mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ TMCProjects/mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -6,5 +6,15 @@ public class SumOfASequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Last number?");
+        int end = Integer.valueOf(scanner.nextLine());
+
+        int sum = 0;
+
+        for (int i = 1; i <= end; i++) {
+            sum += i;
+        }
+        System.out.println("The sum is" + sum);
+
     }
 }

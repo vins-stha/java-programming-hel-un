e6ec6ef5194b2cfd7d9f09fc4677a15d3161df50
vins-stha <shrs_samar@yahoo.com>
2022-06-05 20:13:55 +0300

    Christmas tree with *

--- TMCProjects/mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ TMCProjects/mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -1,26 +1,50 @@
-
 public class AdvancedAstrology {
 
     public static void printStars(int number) {
-        // part 1 of the exercise
+        for (int i = 1; i <= number; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
     }
 
     public static void printSpaces(int number) {
-        // part 1 of the exercise
+        for (int i = 0; i < number; i++) {
+            System.out.print(" ");
+        }
     }
 
     public static void printTriangle(int size) {
-        // part 2 of the exercise
+        int j = 1;
+        for (int i = size; i > 0; i--) {
+
+            printSpaces(i - 1);
+            printStars(j);
+            j++;
+        }
+
     }
 
     public static void christmasTree(int height) {
-        // part 3 of the exercise
+        int j = 0;
+        int l = 0;
+
+        for (int i = height; i > 0; i--) {
+            printSpaces(--height);
+            j = (2 * l) + 1;
+            printStars(j);
+            l++;
+        }
+        int mid = ((j - 3) / 2);
+        printSpaces(mid);
+        System.out.println("***");
+        printSpaces(mid);
+        System.out.println("***");
     }
 
     public static void main(String[] args) {
         // The tests are not checking the main, so you can modify it freely.
 
-        printTriangle(5);
+        printTriangle(3);
         System.out.println("---");
         christmasTree(4);
         System.out.println("---");

9bc6f87df338b125281af78f617b2a535ad85404
vins-stha <shrs_samar@yahoo.com>
2022-06-07 12:59:49 +0300

    started part 4 (1-9)

---
+++ TMCProjects/mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,15 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+
+        Account myAccount = new Account("Arto's account", 100.00);
+
+        myAccount.deposit(20.0);
+
+        System.out.println(myAccount);
+
+    }
+}
---
+++ TMCProjects/mooc-java-programming-i/part04-Part04_08.DecreasingCounter/src/main/java/DecreasingCounter.java
@@ -0,0 +1,26 @@
+
+public class DecreasingCounter {
+
+    private int value;  // an object variable for storing the value of the counter
+
+    public DecreasingCounter(int initialValue) {
+        this.value = initialValue;
+    }
+
+    public void printValue() {
+        // Do not change this code!
+        System.out.println("value: " + this.value);
+    }
+
+    public void decrement() {
+        this.value = this.value - 1;
+        if (this.value < 0) {
+            this.value = 0;
+        }
+    }
+
+    public void reset() {
+        this.value = 0;
+    }
+
+}

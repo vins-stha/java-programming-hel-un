3d2c05e1446080f632cd9e2c737b5d3b91893fc9
vins-stha <shrs_samar@yahoo.com>
2022-06-12 22:13:55 +0300

    part 5 (1-5)

---
+++ TMCProjects/mooc-java-programming-i/part05-Part05_03.Cube/src/main/java/Program.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Experiment with your program here
+        Cube cube = new Cube(4);
+        System.out.println( cube.toString());
+
+    }
+}
--- TMCProjects/mooc-java-programming-i/part05-Part05_04.FitByte/src/main/java/Fitbyte.java
+++ TMCProjects/mooc-java-programming-i/part05-Part05_04.FitByte/src/main/java/Fitbyte.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Fitbyte {
+public class Fitbyte {
+    private int age;
+    private int resting_heart_rate;
+    
+    public Fitbyte(int age, int resting_heart_rate)
+    {
+        this.age = age;
+        this.resting_heart_rate = resting_heart_rate;
+    }
+
+    public double targetHeartRate(double percentageOfMaximum){
+        return ((
+                (this.getMaximumHeartRate()-resting_heart_rate)
+                        *
+                        percentageOfMaximum)
+                + resting_heart_rate);
+    }
+
+    public double getMaximumHeartRate(){
+        return (206.3 - (0.711 * age));
+    }
+
 }

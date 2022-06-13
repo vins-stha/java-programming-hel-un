0975b89ad10c97e271c38108a0513cf045b3588e
vins-stha <shrs_samar@yahoo.com>
2022-06-13 21:43:16 +0300

    part 5 (6-9)

--- TMCProjects/mooc-java-programming-i/part05-Part05_06.OverloadedCounter/src/main/java/Counter.java
+++ TMCProjects/mooc-java-programming-i/part05-Part05_06.OverloadedCounter/src/main/java/Counter.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Counter {
+public class Counter {
+    private int value;
+
+    public Counter() {
+    }
+
+    public Counter(int startValue) {
+        this.value = startValue;
+    }
+
+    public int value() {
+        return value;
+    }
+
+    public void increase() {
+        this.value = this.value + 1;
+    }
+
+    public void decrease() {
+        this.value = this.value - 1;
+    }
+
+    public void increase(int increaseBy) {
+        if (increaseBy > 0) {
+            this.value = this.value + increaseBy;
+        }
+    }
+
+    public void decrease(int decreaseBy) {
+        if (decreaseBy > 0) {
+            this.value = this.value - decreaseBy;
+        }
+    }
+
+
 }

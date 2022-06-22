2c6b712437122a10abf731162d9c70813d9956d7
vins-stha <shrs_samar@yahoo.com>
2022-06-22 23:23:00 +0300

    part 5 -15

--- TMCProjects/mooc-java-programming-i/part05-Part05_15.Archive/src/main/java/Archive.java
+++ TMCProjects/mooc-java-programming-i/part05-Part05_15.Archive/src/main/java/Archive.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class Archive {
+import java.util.ArrayList;
+
+public class Archive {
+    private String name;
+    private String identifier;
+
+    public Archive(String identifier, String name){
+        this.name = name;
+        this.identifier = identifier;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    // check if identifier has been added
+    public boolean checkIdentifierExist(ArrayList<Archive> list)
+    {
+        for(Archive item:list)
+        {
+           if (this.identifier.equals(item.getIdentifier()))
+           {
+               return true;
+           }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return this.identifier + ": " + this.name;
+    }
 }

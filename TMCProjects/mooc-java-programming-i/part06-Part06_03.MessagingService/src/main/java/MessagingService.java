7db0afd0abb86e92e07f1104dd963fe2087773c1
vins-stha <shrs_samar@yahoo.com>
2022-07-06 18:34:50 +0300

    6-03

--- TMCProjects/mooc-java-programming-i/part06-Part06_03.MessagingService/src/main/java/MessagingService.java
+++ TMCProjects/mooc-java-programming-i/part06-Part06_03.MessagingService/src/main/java/MessagingService.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class MessagingService {
+import java.util.ArrayList;
+
+public class MessagingService {
+    private ArrayList<Message> messages;
+
+    public MessagingService() {
+        this.messages = new ArrayList<>();
+    }
+
+    //  adds a message passed as a parameter to the messaging service
+    //  as long as the message content is at most 280 characters long.
+    public void add(Message message) {
+        if (message.getContent().length() <= 280) {
+            this.messages.add(message);
+        }
+    }
+
+    // returns the messages added to the messaging service.
+    public ArrayList<Message> getMessages() {
+        return this.messages;
+    }
 }

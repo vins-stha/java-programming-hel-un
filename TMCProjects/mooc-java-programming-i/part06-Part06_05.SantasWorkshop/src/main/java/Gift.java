923c0279c7f0de8be75b25102f77974891e0f592
vins-stha <shrs_samar@yahoo.com>
2022-07-10 19:51:02 +0300

    part 6-05

--- TMCProjects/mooc-java-programming-i/part06-Part06_05.SantasWorkshop/src/main/java/Gift.java
+++ TMCProjects/mooc-java-programming-i/part06-Part06_05.SantasWorkshop/src/main/java/Gift.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Gift {
+public class Gift {
+    private int weight;
+    private String name;
+
+    public Gift(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Gift: " + this.name + " (" + this.weight + " kg)";
+    }
 }
--- TMCProjects/mooc-java-programming-i/part06-Part06_05.SantasWorkshop/src/main/java/Package.java
+++ TMCProjects/mooc-java-programming-i/part06-Part06_05.SantasWorkshop/src/main/java/Package.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Package {
+import java.util.ArrayList;
+
+public class Package {
+    private ArrayList<Gift> gifts;
+
+    public Package() {
+        this.gifts = new ArrayList<>();
+    }
+
+    // adds the gift passed as a parameter to the package
+    public void addGift(Gift gift) {
+        gifts.add(gift);
+    }
+
+    // which returns the total weight of the package's gifts
+    public int totalWeight() {
+        int total = 0;
+        for (Gift gift : gifts) {
+            total += gift.getWeight();
+
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        String message="";
+        for(Gift gift:this.gifts){
+            message += gift.toString() + "\n";
+        }
+        return "Gifts inside package: \n" + message;
+    }
 }
